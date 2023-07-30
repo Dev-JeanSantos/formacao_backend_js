66dc64cea15e3801fda3d375b27338f413565d0b
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-30 13:31:31 -0300

    alterar um array com map

--- javascript/modulo2/arrays/map.js
+++ javascript/modulo2/arrays/map.js
@@ -5,4 +5,10 @@ const notasAtualizadas =  notas.map((nota) => {
     return nota + 1 >= 10 ? 10 : nota + 1 ;
 })
 
-console.log(notasAtualizadas)
+console.log(notasAtualizadas);
+
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+console.log(nomes);
+const nomes_padronizados = nomes.map((nome) =>nome.toUpperCase());
+console.log(nomes_padronizados);
