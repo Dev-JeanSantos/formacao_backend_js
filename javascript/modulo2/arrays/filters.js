30e09d7d95aea8c6ca3e6935392e2e55cd789089
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-30 15:14:52 -0300

    filters

--- javascript/modulo2/arrays/filters.js
+++ javascript/modulo2/arrays/filters.js
@@ -6,3 +6,19 @@ const reprovados = alunos.filter((_, indice) => {
 })
 
 console.log(reprovados);
+
+
+const salaJS = [7,8,8,7,10,6.5,4,10,7];
+const salaJava = [6,5,8,9,5,6];
+const salaPython = [7,3.5,8,9.5];
+
+function calculaMedia(notasDaSala){
+    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
+        return acumulador + nota;
+    }, 0);
+
+    const media = somaDasNotas / notasDaSala.lenght
+    return media;
+}
+
+console.log(`A media da sala de JS é ${calculaMedia(salaJS)}`)
