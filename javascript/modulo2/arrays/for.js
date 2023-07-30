043ffcb6e4cdca3773926fe4e799ce1b05bd753b
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-30 13:04:49 -0300

    for of

--- javascript/modulo2/arrays/for.js
+++ javascript/modulo2/arrays/for.js
@@ -1,5 +1,23 @@
-const numeros = [100, 200, 300, 400, 500, 600, 700, 800,900,1000]
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 
-for (let indice = 0; indice < numeros.length; indice++){
+for (let indice = 0; indice < numeros.length; indice++) {
     console.log(numeros[indice])
 }
+
+const notas = [10, 6.5, 8, 7.5, 10];
+let somatorio = 0
+
+for (let i = 0; i < notas.length; i++) {
+    somatorio += notas[i]
+
+}
+const media = somatorio / notas.length
+console.log(`Somatorio notas: ${somatorio}`)
+console.log(`media das notas com for comum: ${media}`)
+
+let somaDasNotas = 0
+for (let nota of notas) {
+    somaDasNotas +=nota
+}
+const media2 = somaDasNotas / notas.length
+console.log(`media com for of: ${media2}`)
