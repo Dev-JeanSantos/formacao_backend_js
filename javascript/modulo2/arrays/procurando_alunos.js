003cb6aa124282055c088a6d51c410a796fe0f55
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-30 12:21:43 -0300

    desestruturação

--- javascript/modulo2/arrays/procurando_alunos.js
+++ javascript/modulo2/arrays/procurando_alunos.js
@@ -5,9 +5,16 @@ const listaTypeDistintos = [alunos, notas]
 
 function exibeNomeENotas(aluno){
     if(listaTypeDistintos[0].includes(aluno)){
-        const indiceAluno = listaTypeDistintos[0].indexOf(aluno);
-        console.log(`Indice do aluno: ${indiceAluno}`);
-        const notaAluno = listaTypeDistintos[1][indiceAluno]
+    
+        // listaAlunos = listaTypeDistintos[0]
+        // listaNotas = listaTypeDistintos[1]
+        
+        //Desestruturação
+        const [listaAlunos, listaNotas] =  listaTypeDistintos;
+        
+        const indiceAluno = listaAlunos.indexOf(aluno);       
+        const notaAluno = listaNotas[indiceAluno]
+
         console.log(`Aluno ${aluno} está cadastrado e sua nota é ${notaAluno}`);
     }else{
         console.log(`Aluno ${aluno} não cadastrado!`);
