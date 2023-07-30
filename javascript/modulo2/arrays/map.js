34c93628ffd12bfbc138dafb37d462b88621a78a
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-30 13:26:12 -0300

    map

---
+++ javascript/modulo2/arrays/map.js
@@ -0,0 +1,8 @@
+const notas = [10, 6.5, 8, 7.5, 11];
+console.log(notas)
+
+const notasAtualizadas =  notas.map((nota) => {
+    return nota + 1 >= 10 ? 10 : nota + 1 ;
+})
+
+console.log(notasAtualizadas)
