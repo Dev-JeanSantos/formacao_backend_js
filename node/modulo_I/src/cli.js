7254c67c6f664e6e19b1c2f5d8fc3081f0f9e172
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-31 22:29:16 -0300

    refatorando o cli.js

--- node/modulo_I/src/cli.js
+++ node/modulo_I/src/cli.js
@@ -1,4 +1,11 @@
 import  getArchivePromissesByAssync from './index.js';
+import chalk from 'chalk';
 
 const path = process.argv;
-getArchivePromissesByAssync(path[2]);
+
+async function processText(path){
+    const result = await getArchivePromissesByAssync(path[2]);
+    console.log(chalk.yellow('Lista de Links'), result);
+}
+
+processText(path);
--- node/modulo_I/src/index.js
+++ node/modulo_I/src/index.js
@@ -6,7 +6,7 @@ function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
   }
 
 function correctError(error){
@@ -19,7 +19,7 @@ async function getArchivePromissesByAssync(pathArchive){
     try{
         const encoding = 'utf-8';
         const text = await fs.promises.readFile(pathArchive, encoding)
-        console.log(extraiLinks(text));
+         return extraiLinks(text);
     }catch(error){
         correctError(error)
     }
